3e8aa1777851b5e04620009fc57b5c87135d31d0
kmaurice012 <kmaurice012@gitlab.com>
2024-03-27 11:17:11 +0300

    Add MenuLink Layout And Styles

---
+++ src/app/ui/dashboard/sidebar/menuLink/menuLink.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Link from "next/link";
+import styles from "./menuLink.module.css";
+import { usePathname } from "next/navigation";
+
+interface MenuItem {
+  path: string;
+  icon: JSX.Element;
+  title: string;
+}
+
+const MenuLink: React.FC<{ item: MenuItem }> = ({ item }) => {
+    const pathname = usePathname()
+  return (
+    <Link href={item?.path} className={`${styles.container} ${pathname === item.path && styles.active}`}>
+      {item.icon}
+      {item.title}
+    </Link>
+  );
+};
+
+export default MenuLink;
