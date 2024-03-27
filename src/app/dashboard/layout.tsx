97f243378b15292722bbfaeb210d3e4604dc338c
kmaurice012 <kmaurice012@gitlab.com>
2024-03-27 11:13:41 +0300

    Add Dashboard Layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,18 @@
 import Navbar from "../ui/dashboard/navbar/navbar";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
+import style from '../ui/dashboard/dashboard.module.css'
 
 interface LayoutProps {
-    children: React.ReactNode;
-  }
-  
+  children: React.ReactNode;
+}
 
-const Layout : React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      <div>
+    <div className={style.container}>
+      <div className={style.menu}>
         <Sidebar />
       </div>
-      <div>
+      <div className={style.content}>
         <Navbar />
         {children}
       </div>
