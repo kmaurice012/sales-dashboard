b4d6f163fca031ebab6fc166fabcb3013f59a235
kmaurice012 <kmaurice012@gitlab.com>
2024-03-27 14:26:24 +0300

    Add Footer To Layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../ui/dashboard/navbar/navbar";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import style from '../ui/dashboard/dashboard.module.css'
+import Footer from "../ui/dashboard/footer/footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -15,6 +16,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className={style.content}>
         <Navbar />
         {children}
+        <Footer />
       </div>
     </div>
   );
